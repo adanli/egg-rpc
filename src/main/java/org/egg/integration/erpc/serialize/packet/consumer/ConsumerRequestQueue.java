0360badad5b31828d347b3eda025eebcd1e171a9
adanli <adanli@126.com>
2020-10-19 01:22:25 +0800

    添加线程唤醒功能，防止线程假死

--- src/main/java/org/egg/integration/erpc/serialize/packet/consumer/ConsumerRequestQueue.java
+++ src/main/java/org/egg/integration/erpc/serialize/packet/consumer/ConsumerRequestQueue.java
@@ -3,6 +3,7 @@ package org.egg.integration.erpc.serialize.packet.consumer;
 import org.egg.integration.erpc.annotation.Service;
 import org.egg.integration.erpc.constant.RequestQueue;
 import org.egg.integration.erpc.serialize.packet.Packet;
+import sun.nio.ch.Interruptible;
 
 import java.util.LinkedList;
 
@@ -27,22 +28,49 @@ public class ConsumerRequestQueue {
     }
 
     class ConsumerRequest implements Runnable {
+        private Interruptible interruptor = null;
 
         @Override
         public void run() {
             while (start) {
-                Thread.currentThread().interrupt();
-                while (packets.size() > 0) {
-                    synchronized (RequestQueue.REQUEST_QUEUE_LOCK) {
-                        Packet packet = packets.pollLast();
-                        System.out.println("消费" + packet);
-                    }
+                this.begin();
+                doConsumer();
+                this.end();
+            }
+        }
+
+        private void doConsumer() {
+            while (packets.size() > 0) {
+                synchronized (RequestQueue.REQUEST_QUEUE_LOCK) {
+                    Packet packet = packets.pollLast();
+                    System.out.println("消费" + packet);
                 }
             }
         }
 
+        private void begin() {
+            if(interruptor == null) {
+                interruptor = new Interruptible() {
+                    @Override
+                    public void interrupt(Thread thread) {
 
+                    }
+                };
+            }
+            blockOn(interruptor);
+            Thread me = Thread.currentThread();
+            if(me.isInterrupted()) {
+                interruptor.interrupt(me);
+            }
+        }
+
+        private void end() {
+            blockOn(null);
+        }
 
     }
 
+    private static void blockOn(Interruptible interrupt) {
+        sun.misc.SharedSecrets.getJavaLangAccess().blockedOn(Thread.currentThread(), interrupt);
+    }
 }
